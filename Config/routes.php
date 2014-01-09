f4c7ca0eb37248a0e6b111c952062512c1cfd450
fm-labs <flowmotionlabs@gmail.com>
2014-01-09 13:08:24 +0100

    updated routes

--- Config/routes.php
+++ Config/routes.php
@@ -1,1 +1,12 @@
 <?php
+Router::connect('/facebook/login',
+    array('plugin'=>'facebook','controller'=>'facebook','action'=>'login'));
+
+Router::connect('/facebook/logout',
+    array('plugin'=>'facebook','controller'=>'facebook','action'=>'logout'));
+
+Router::connect('/facebook/connect',
+    array('plugin'=>'facebook','controller'=>'facebook','action'=>'connect'));
+
+Router::connect('/facebook/disconnect',
+    array('plugin'=>'facebook','controller'=>'facebook','action'=>'disconnect'));
