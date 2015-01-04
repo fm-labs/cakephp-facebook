cdbcbcbbdb7bc5c3e97d37792e993b4a3e3880d6
fm-labs <flowmotionlabs@gmail.com>
2015-01-04 14:26:20 +0100

    minor fixes; marked files under Model/ as DRAFT

--- Model/FacebookGraph.php
+++ Model/FacebookGraph.php
@@ -1,21 +1,46 @@
 <?php
 App::uses('FacebookAppModel', 'Facebook.Model');
 
+/**
+ * Class FacebookGraph
+ *
+ * !!! DRAFT !!!
+ * !!! DO NOT USE IN PRODUCTION !!!
+ */
 class FacebookGraph extends FacebookAppModel {
 
+    //@REFACTOR
 	public function find($type = 'first', $query = array()) {
 		return $this->get($type, $query);
 	}
 
+    //@DEPRECATED
 	public function get($path, $params) {
 		return $this->getDataSource()->query($this, $path, 'GET', $params);
 	}
 
+    //@DEPRECATED
 	public function post($path, $params = array()) {
 		return $this->getDataSource()->query($this, $path, 'POST', $params);
 	}
 
+    //@DEPRECATED
 	public function remove($path, $params = array()) {
 		return $this->getDataSource()->query($this, $path, 'DELETE', $params);
 	}
+
+    //@TODO
+    public function read($fields = null, $id = null) {
+        return false;
+    }
+
+    //@TODO
+    public function save($data = null, $validate = true, $fieldList = array()) {
+        return false;
+    }
+
+    //@TODO
+    public function delete($id = null, $cascade = true) {
+        return false;
+    }
 }
--- Model/FacebookUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-App::uses('FacebookGraph', 'Facebook.Model');
-
-class FacebookUser extends FacebookGraph {
-
-	public function me() {
-		return $this->find('/me');
-	}
-}
